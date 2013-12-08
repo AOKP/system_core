omnirom/android_system_core_old

healthd: who really needs this dmesg spilling by default
Change-Id: I2cf1a013515f8d50c26c80bb810ee1171575395c

healthd/Android.mk
@@ -4,6 +4,10 @@ ifneq ($(BUILD_TINY_ANDROID),true)
 
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(strip $(HEALTHD_CHATTY_MODE)),true)
+    common_cflags += -DHEALTHD_SILENCE_LOG
+endif
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_default.cpp
 LOCAL_MODULE := libhealthd.default

AOKP/system_core

healthd/Android.mk
@@ -7,6 +7,11 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(USE_DEVICE_SPECIFIC_LIBHEALTHD),true)
 
 include $(CLEAR_VARS)
+
+ifneq ($(strip $(HEALTHD_CHATTY_MODE)),true)
+    LOCAL_CFLAGS += -DHEALTHD_SILENCE_LOG
+endif
+
 LOCAL_SRC_FILES := healthd_board_default.cpp
 LOCAL_MODULE := libhealthd.default
 include $(BUILD_STATIC_LIBRARY)